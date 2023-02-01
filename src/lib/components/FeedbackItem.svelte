c8cc1d1c8d9235ec4140895cedf9a71849b7d0d0
Bruno Torrabadella <bruktorra@gmail.com>
2023-02-01 14:24:25 -0300

    Props to FeedbackItem and basic style

---
+++ src/lib/components/FeedbackItem.svelte
@@ -0,0 +1,29 @@
+<script>
+    import Card from "../ui/Card.svelte";
+    export let fb = {};
+
+</script>
+
+<Card>
+    <div class="num-list">
+        {fb.id}
+    </div>
+    <div class="close-icon">X</div>
+    <div class="content">{fb.text}</div>
+</Card>
+
+<style>
+    .close-icon {
+        position: absolute;
+        cursor: pointer;
+        padding: 0.5rem 1rem;
+        top: 0;
+        right: 0;
+    }
+    .num-list {
+        position: absolute;
+        padding: 0.5rem 1rem;
+        top: 0;
+        left: 0;
+    }
+</style>
