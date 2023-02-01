0481b01ba82a4c907f7586feaee6224d81959f41
Bruno Torrabadella <bruktorra@gmail.com>
2023-02-01 14:23:16 -0300

    single card for each feedback UI

---
+++ src/lib/ui/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+    <slot></slot>
+</div>
+<style>
+    .card {
+        background-color: purple;
+        color: white;
+        border-radius: 1rem;
+        padding: 1.5em 3em;
+        margin: 1rem 0;
+        position: relative;
+
+    }
+</style>
